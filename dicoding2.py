1eee8df19db9b085841bbf29b7a5220d70932a7d
micz044 <alvaromichael044@gmail.com>
2023-09-07 09:38:38 +0800

    Bukan Ateri Dari Hactiv8

--- dicoding2.py
+++
@@ -1,9 +0,0 @@
-def cetak(a=10):
-    hasil = print("isi dari a adalah",format(a))
-    return cetak
-
-
-cetak()
-
-
-
--- kode2.py
+++
@@ -1,9 +0,0 @@
-panas = False
-
-print("Saya pergi kerja","dengan semangat" 
-      if panas else ",tapi boong: v")
-
-umur = int(input("masukkan umur anda = "))
-
-hasil = "masih muda ente" if umur <21 else "deh Tua bangka: v"
-print(hasil)
